c7d3913e78cae01b0d13a45cc4c99cc0d24295d0
Duncan Mazza <dmazza@olin.edu>
2019-03-12 21:56:54 -0400

    Add changes to take advantage of NewCompositePlanes

--- detecting_planes/build_planes.py
+++ detecting_planes/build_planes.py
@@ -6,9 +6,9 @@ from mpl_toolkits import mplot3d
 from making_test_stl import make_test_cube, make_any_stl
 import utility_funcs as uf
 from classes import Stl
-from classes import CompositePlanes
+from classes import NewCompositePlanes
 
-THRESHOLD = 0.01 # threshold for difference between facet/plane normal uf.crosses
+THRESHOLD = 0.5 # threshold for difference between facet/plane normal uf.crosses
 
 cube = make_test_cube() # test stl
 
@@ -41,12 +41,13 @@ if __name__=="__main__":
         print("Invalid response; will not plot the stl. \n")
 
     stl_object.make_all() # generate all object attributes
-    comp_planes = CompositePlanes(stl_object.adj_mat)
 
     print("Calculating planes in stl...")
+    comp_planes = NewCompositePlanes(stl_object)
+    comp_planes.build_planes()
 
-    for i in range(len(stl_object.coord_mat)):
-        comp_planes.add_facet(stl_object.coord_mat[i], i)
+    # for i in range(len(stl_object.coord_mat)):
+    #     comp_planes.add_facet(stl_object.coord_mat[i], i)
 
     print("Planes calculated!")
     print(comp_planes)
