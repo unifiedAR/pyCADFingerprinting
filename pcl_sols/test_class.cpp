d1bd2d90a790aca9546294b5c827b5af6f822d7e
Duncan Mazza <dmazza@olin.edu>
2019-04-24 01:40:11 -0400

    Add first code for testing header file

---
+++ pcl_sols/test_class.cpp
@@ -0,0 +1,12 @@
+//
+// Created by duncan on 4/24/19.
+//
+
+#include <iostream>
+#include "correspGroup.h"
+
+int main {
+    cg = CorrespGroup(2, "milk.pcd", "milk_cartoon_all_small_clorox.pcd")
+
+    return 0;
+};
