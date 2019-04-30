196cd514e942c437b535efb38cb065e478a1cdc0
Duncan Mazza <dmazza@olin.edu>
2019-04-30 14:50:34 -0400

    Add #includes for debugging in docker container

--- pcl_sols/src/correspGroup.h
+++ pcl_sols/src/correspGroup.h
@@ -5,7 +5,20 @@
 #ifndef CORRESPONDENCE_GROUPING_CORRESPGROUP_H
 #define CORRESPONDENCE_GROUPING_CORRESPGROUP_H
 
-
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/correspondence.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/features/shot_omp.h>
+#include <pcl/features/board.h>
+#include <pcl/filters/uniform_sampling.h>
+#include <pcl/recognition/cg/hough_3d.h>
+#include <pcl/recognition/cg/geometric_consistency.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/kdtree/impl/kdtree_flann.hpp>
+#include <pcl/common/transforms.h>
+#include <pcl/console/parse.h>
 #include <iostream>
 
 
