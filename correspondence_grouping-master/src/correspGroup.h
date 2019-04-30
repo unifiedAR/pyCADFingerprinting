cfc56e5f8e8f52c3b194eadf3633109a3a1af70d
Duncan Mazza <dmazza@olin.edu>
2019-04-30 15:31:03 -0400

    Add code for debugging in docker container

---
+++ correspondence_grouping-master/src/correspGroup.h
@@ -0,0 +1,18 @@
+//
+// Created by duncan on 4/30/19.
+//
+
+
+#ifndef CORRESPONSEGROUPING_CORRESPGROUP_H
+#define CORRESPONSEGROUPING_CORRESPGROUP_H
+
+
+namespace N {
+    class CorrespGroup {
+    public:
+        struct find(model_filename_, scene_filename_);
+    };
+}
+
+
+#endif //CORRESPONSEGROUPING_CORRESPGROUP_H
