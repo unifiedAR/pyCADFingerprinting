3df28efe13ba39ea8b2a6ead40c2b4369c74dc8b
Duncan Mazza <dmazza@olin.edu>
2019-04-30 19:35:06 -0400

    Fix file paths for the point clouds

--- correspondence_grouping-master/src/doStuff.cpp
+++ correspondence_grouping-master/src/doStuff.cpp
@@ -8,6 +8,6 @@ using namespace N;
 
 int main () {
     CorrespGroup model;
-    model.find("milk.pcd", "milk_cartoon_all_small_clorox.pcd");
+    model.find("../test/milk.pcd", "../test/milk_cartoon_all_small_clorox.pcd");
     return 0;
 };
