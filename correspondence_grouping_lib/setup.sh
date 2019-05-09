f96d2ab728ba5d2d9989761656635859e3ace66b
Duncan Mazza <dmazza@olin.edu>
2019-05-09 17:00:24 -0400

    Add new script for testing

--- correspondence_grouping_lib/setup.sh
+++ correspondence_grouping_lib/setup.sh
@@ -1,4 +1,16 @@
-rm -rf build;
+#!/usr/bin/env bash
+cd /home/
+apt-get update;
+apt-get install curl;
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py;
+pip install virtualenv;
+mkdir python-virtual-environments && cd python-virtual-environments
+apt-get install python3-venv
+python3 -m venv env;
+source env/bin/activate;
+cd /home/
+git clone https://github.com/unifiedAR/pyCADFingerprinting.git;
+cd pyCADFingerprinting/;
 mkdir build;
 cd build;
 cmake ..;
