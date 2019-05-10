bf188ca963152815dd2296c4f7b4c05e08c10ea0
Duncan Mazza <dmazza@olin.edu>
2019-05-10 15:10:26 -0400

    Update the cd commands

--- correspondence_grouping_lib/setup.sh
+++ correspondence_grouping_lib/setup.sh
@@ -8,14 +8,13 @@ mkdir python-virtual-environments && cd python-virtual-environments
 apt-get install python3-venv
 python3 -m venv env;
 source env/bin/activate;
-cd /home/
-git clone https://github.com/unifiedAR/pyCADFingerprinting.git;
-cd pyCADFingerprinting/;
+cd /home/pyCADFingerprinting/correspondence_grouping_lib;
 mkdir build;
 cd build;
 cmake ..;
 make;
-mv libCorrespGroup.so ../
-cd ..;
+mv libCorrespGroup.so ../src/
+cd ../src/;
 pip3 install cython;
+
 python setup.py build_ext --inplace;
