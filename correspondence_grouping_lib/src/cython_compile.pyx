2eb231933de503bd4516aefed83c8905166917f8
Duncan Mazza <dmazza@olin.edu>
2019-05-10 15:45:11 -0400

    Fix arguments leftover from template code

--- correspondence_grouping_lib/src/cython_compile.pyx
+++ correspondence_grouping_lib/src/cython_compile.pyx
@@ -3,5 +3,5 @@ from libcpp.string cimport string
 cdef extern from "correspGroup.h":
     void find (char *model_filename, char *scene_filename);
 
-def pyCorrespGroup(name: bytes) -> None:
+def pyCorrespGroup(model_filename: bytes, scene_filename: bytes) -> None:
     find(name)
