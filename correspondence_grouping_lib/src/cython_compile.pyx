073e8fa21710aa02244538276582d833fc3c2090
Duncan Mazza <dmazza@olin.edu>
2019-05-10 15:34:26 -0400

    Make the cpp function callable

--- correspondence_grouping_lib/src/cython_compile.pyx
+++ correspondence_grouping_lib/src/cython_compile.pyx
@@ -3,5 +3,5 @@ from libcpp.string cimport string
 cdef extern from "correspGroup.h":
     void find (char *model_filename, char *scene_filename);
 
-def py_hello(name: bytes) -> None:
-hello(name)
+def pyCorrespGroup(name: bytes) -> none;
+    find(name)
