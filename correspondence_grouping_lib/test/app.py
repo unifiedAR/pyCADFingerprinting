4b27c353126c18f32c4b8c093d56bcd1add2bb5b
Duncan Mazza <dmazza@olin.edu>
2019-05-11 11:29:17 -0400

    CHanges for testing web app in docker

--- correspondence_grouping_lib/test/app.py
+++ correspondence_grouping_lib/test/app.py
@@ -1,6 +1,26 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+
+class WebApplication(Flask):
+    """
+    A wrapper for a Flask application to simplify app configuration and launching.
+    """
+
+    def __init__(self, app_name=None, debug=False):
+        # Call __init__ from the Flask superclass
+        super().__init__(app_name or __name__)
+
+        # Set configuration variables
+        self.debug = debug
+        self.config['SECRET_KEY'] = os.urandom(16)
+        Session(self)  # for the cookies
+
+    def listen(self, **options):
+        """ Asks Flask to begin listening to HTTP requests, with options if given. """
+        self.run(options.get('host', "0.0.0.0"), options.get('port', 3000), options, use_reloader=False)
+
+
 @app.route('/')
 def index():
     return 'Index page'
@@ -16,3 +36,7 @@ def matrix():
     matrix_data = request.get_data().decode()
     print(matrix_data)
     return 'none'
+
+if __name__ == "__main__":
+    app = WebApplication("spatial_computing_lab")
+    app.listen(port=8080)
