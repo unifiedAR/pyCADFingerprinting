4f3be6e8765120831d86d6a1b8f2e056b008bf25
Jonathan Kelley <jkelleyrtp@gmail.com>
2019-05-11 15:14:48 -0400

    Testing .so

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from flask_session import Session
 app = Flask(__name__)
 
 
+'''
+[ ] Make sure .so builds correctly
+[ ] Call .so file from python
+
+[ ] Receive point cloud data in the form of PCD
+
+[ ] Return transform in the form of a matrix/quaternion
+
+'''
+
+
 class WebApplication(Flask):
     """
     A wrapper for a Flask application to simplify app configuration and launching.
@@ -36,6 +47,10 @@ def matrix():
     matrix_data = request.get_data().decode()
     print(matrix_data)
     return 'none'
+
+
+
+
 
 if __name__ == "__main__":
     app = WebApplication("spatial_computing_lab")
