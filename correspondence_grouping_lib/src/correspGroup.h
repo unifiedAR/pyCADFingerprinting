537e747896cb82e2168e6cf642cc19354338b738
Duncan Mazza <dmazza@olin.edu>
2019-05-11 12:22:59 -0400

    Return function arguments to original type

--- correspondence_grouping_lib/src/correspGroup.h
+++ correspondence_grouping_lib/src/correspGroup.h
@@ -5,9 +5,11 @@
 #ifndef CORRESPONSEGROUPING_CORRESPGROUP_H
 #define CORRESPONSEGROUPING_CORRESPGROUP_H
 
+#include <string.h>
+
 class CorrespGroup {
 public:
-    void find (char *model_filename, char *scene_filename);
+    void find (std::string model_filename, std::string scene_filename);
 };
 
 #endif //CORRESPONSEGROUPING_CORRESPGROUP_H
