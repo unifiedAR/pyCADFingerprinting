1a6de3adb7ee5718018567f392ab898818d028f4
Duncan Mazza <dmazza@olin.edu>
2019-05-11 11:17:59 -0400

    Remove cython-associated files

--- correspondence_grouping_lib/src/cython_compile.pyx
+++
@@ -1,8 +0,0 @@
-# cdef extern from "correspGroup.h":
-#     void CorrespGroup :: find (char *model_filename, char *scene_filename);
-
-cdef extern from "correspGroup.h":
-    void find (char *model_filename, char *scene_filename);
-
-def pyCorrespGroup(model_filename: bytes, scene_filename: bytes) -> None:
-    find(model_filename, scene_filename)
