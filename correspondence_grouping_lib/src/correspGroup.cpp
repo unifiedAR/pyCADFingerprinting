52dc0a2647e9c2df9628beffe5259d1b85b02d78
Duncan Mazza <dmazza@olin.edu>
2019-05-11 11:58:52 -0400

    Fix missing bracket for function declaration

--- correspondence_grouping_lib/src/correspGroup.cpp
+++ correspondence_grouping_lib/src/correspGroup.cpp
@@ -74,30 +74,27 @@ void CorrespGroup :: find (char *model_filename, char *scene_filename)
         double res = 0.0;
         int n_points = 0;
         int nres;
-        std::vector<int> indices (2);
-        std::vector<float> sqr_distances (2);
-        pcl::search::KdTree<PointType> tree;
-        tree.setInputCloud (cloud);
+        std::vector<int> indices(2);
+        std::vector<float> sqr_distances(2);
+        pcl::search::KdTree <PointType> tree;
+        tree.setInputCloud(cloud);
 
-        for (size_t i = 0; i < cloud->size (); ++i)
-        {
-            if (! std::isfinite ((*cloud)[i].x))
-            {
+        for (size_t i = 0; i < cloud->size(); ++i) {
+            if (!std::isfinite((*cloud)[i].x)) {
                 continue;
             }
             //Considering the second neighbor since the first is the point itself.
-            nres = tree.nearestKSearch (i, 2, indices, sqr_distances);
-            if (nres == 2)
-            {
-                res += sqrt (sqr_distances[1]);
+            nres = tree.nearestKSearch(i, 2, indices, sqr_distances);
+            if (nres == 2) {
+                res += sqrt(sqr_distances[1]);
                 ++n_points;
             }
         }
-        if (n_points != 0)
-        {
+        if (n_points != 0) {
             res /= n_points;
         }
         return res;
+    }
 
     if (use_cloud_resolution_)
     {
