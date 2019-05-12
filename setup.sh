ec78e6913e1f8aa4736ff5443cd3434e732ad2b5
Duncan Mazza <dmazza@olin.edu>
2019-05-12 15:08:36 -0400

    Update dependencies for python-pcl

--- setup.sh
+++ setup.sh
@@ -4,9 +4,12 @@ apt-get update;
 apt-get install curl;
 apt-get install python3-dev;
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py;
+pip install --upgrade pip;
 pip install virtualenv;
+pip install numpy;
 pip install flask;
 pip install flask_session;
+pip install cython;
 mkdir python-virtual-environments && cd python-virtual-environments
 apt-get install python3-venv
 python3 -m venv env && source env/bin/activate;
@@ -20,4 +23,4 @@ cd build;
 cmake ..;
 make -j8;
 mv libCorrespGroup.so ../src/correspGroup
-cd /home/pyCADFingerrinting/
+cd /home/pyCADFingerprinting/
