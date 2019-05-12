1c1d21f8a34293b9f84c2578b7112537947fe2cd
Duncan Mazza <dmazza@olin.edu>
2019-05-12 14:49:11 -0400

    Add parsing data capabilities to src/

---
+++ src/parse_data.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pcl
+import os
+
+
+def text_to_pcd(mesh):
+    """
+    Converts text in the form of a csv (delimiter='c') that represents a 3D point cloud into a pcl.PointCLoud
+    :param mesh: text in the form of a csv (delimiter='c') from a Unity mesh
+    :return: PointCloud object
+    """
+    mesh = mesh[6:len(mesh) - 2]  # remove 'mesh=c ...' and '... \n from the beginning and end of the string
+    mesh_list = mesh.split('c')
+    mesh_array = np.asarray(mesh_list)
+    mesh_mat = np.reshape(mesh_array, (int(len(mesh_list) / 3), 3))
+    mesh_mat = mesh_mat.astype('float32')
+    print(mesh_mat)
+    return pcl.PointCloud(mesh_mat)
+
+def save_text_to_pcd(mesh, path='auto', file_name='model_mesh.pcd'):
+    """
+    Saves a text of a mesh from Unity to a .pcd file to be used for correspondence grouping
+    :param mesh: text in the form of a csv (delimiter='c') from a Unity mesh
+    :param path: directory to save the pcd
+    :param file_name: name of the pcd file
+    :return: void
+    """
+    if path == 'auto':
+        file_path = os.path.join(os.getcwd(), 'mesh_folder', file_name)
+    else:
+        file_path = os.path.join(path, file_name)
+    pc = text_to_pcd(mesh)
+    pcl.save(pc, file_path, format='pcd')
+
+if __name__ == '__main__':
+    mesh = open('test_mesh.txt', 'r+')
+    mesh = mesh.read()
+    pc = save_text_to_pcd(mesh)
+
+
--- test/duncan_test/parse_data_test.py
+++ test/duncan_test/parse_data_test.py
@@ -1,11 +1,12 @@
-import numpy as np  
+import numpy as np
 import pcl
 import os
+
 
 def load_mesh(mesh_filename='test_mesh.txt'):
     mesh = open(mesh_filename, 'r+')
     mesh = mesh.read()
-    mesh = mesh[6:len(mesh)-2]  # remove 'mesh=c ...' and '... \n from the beginning and end of the string
+    mesh = mesh[6:len(mesh) - 2]  # remove 'mesh=c ...' and '... \n from the beginning and end of the string
     mesh_list = mesh.split('c')
     mesh_array = np.asarray(mesh_list)
     mesh_mat = np.reshape(mesh_array, (int(len(mesh_list) / 3), 3))
@@ -13,6 +14,7 @@ def load_mesh(mesh_filename='test_mesh.txt'):
     print(mesh_mat)
     pc = pcl.PointCloud(mesh_mat)
     return pc
+
 
 if __name__ == '__main__':
     pc = load_mesh()
