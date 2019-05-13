ef79f47e4d26c2d0f9b7559990989615ff374cf0
Duncan Mazza <dmazza@olin.edu>
2019-05-12 22:52:48 -0400

    Fix script (now in working state)

--- src/stl_to_pcd.py
+++ src/stl_to_pcd.py
@@ -3,6 +3,8 @@ import numpy as np
 from stl import mesh
 
 mesh = mesh.Mesh.from_file('mesh_folder/model.stl')
-mesh = mesh.astype('float32')
-
+npmesh = mesh.points.reshape(len(mesh.points)*3, 3)
+npmesh = np.unique(npmesh, axis=0)
+npmesh = npmesh.astype('float32')
+pc = pcl.PointCloud(npmesh)
 pcl.save(pc, 'mesh_folder/mesh.pcd', format='pcd')
