4ee5db19b3d35589b40b190a80ec4caacd00a489
Jonathan Kelley <jkelleyrtp@gmail.com>
2019-05-12 21:47:05 -0400

    jsonifer

--- test/python-mesh/dumpjson.py
+++ test/python-mesh/dumpjson.py
@@ -6,34 +6,35 @@ from scipy.spatial.transform import Rotation as R
 
 #%%
 
-item = "success=true;rotation= 1.000,-0.000, 0.000, 0.000, 1.000, 0.000, 0.000, 0.000, 1.000;translation=0.000, 0.000, 0.000;"
-obj = {}
-#obj['success'] = 
-item.find("success")
+def cg_to_json(cg_output):
+    """
 
 
-idx = 0
-lastidx = 0
-for ltr in item:
-    idx+=1
-    if ltr == ";":
-        sub = item[lastidx:idx]
-        a,b,c, = str.partition(sub, "=")
-        lastidx = idx
-        print(a,c[:-1])
-        obj[a] = c[:-1].replace(" ", "")
+    """
+
+    obj = {}
+    idx = 0
+    lastidx = 0
+    for ltr in item:
+        idx+=1
+        if ltr == ";":
+            sub = item[lastidx:idx]
+            a,b,c, = str.partition(sub, "=")
+            lastidx = idx
+            print(a,c[:-1])
+            obj[a] = c[:-1].replace(" ", "")
 
 
-        if(a == "rotation"):
-            rotmatrix = np.fromstring(c[:-1], sep=',').reshape(3,3)
-            quaternion = R.from_rotvec(rotmatrix).as_quat()
-           # print(quaternion)
-            obj[a] = quaternion.tolist()
+            if(a == "rotation"):
+                rotmatrix = np.fromstring(c[:-1], sep=',').reshape(3,3)
+                quaternion = R.from_rotvec(rotmatrix).as_quat()
+            # print(quaternion)
+                obj[a] = quaternion.tolist()
 
-        if(a == "translation"):
-            obj[a] = np.fromstring(c[:-1], sep=',').tolist()
+            if(a == "translation"):
+                obj[a] = np.fromstring(c[:-1], sep=',').tolist()
 
-js = print(json.dumps(obj))
+    return json.dumps(obj)
 
 
 
